0000d9b118a06fd4ce71cc92b28dc951257bfab1
Kevin_QQ <ezhqing@163.com>
2018-01-16 21:30:30 +0800

    font-awesome5

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,33 +2,32 @@
     <f7-page>
         <f7-navbar>
             <f7-nav-left>
-                <f7-link class="panel-open" open-panel="left" icon-material="menu">
-                </f7-link>
+                <f7-link class="panel-open" open-panel="left" icon="fas fa-bars"></f7-link>
             </f7-nav-left>
             <div class="title">Framework7-Vue</div>
             <f7-nav-right>
-                <f7-link class="searchbar-enable" data-searchbar=".searchbar-components" icon-material="search"></f7-link>
+                <f7-link class="searchbar-enable" data-searchbar=".searchbar-components" icon="fas fa-search"></f7-link>
             </f7-nav-right>
             <f7-searchbar class="searchbar-components" search-container=".components-list" search-in="a" expandable></f7-searchbar>
         </f7-navbar>
         <f7-block-title>Introduction</f7-block-title>
         <f7-block inner>
             <p>This is template for Framework7 v2, Vue and Webpack.</p>
-            <p>Here use Material Icons, you can replace other icons as you like.</p>
+            <p>Here use Font-awesome 5 Icons, you can replace other icons as you like.</p>
         </f7-block>
         <f7-block-title class="searchbar-found">Links</f7-block-title>
         <f7-list class="components-list searchbar-found">
             <f7-list-item link="/form/" title="Form">
-                <f7-icon slot="media" material="view_agenda"></f7-icon>
+                <f7-icon slot="media" icon="far fa-list-alt fa-fw"></f7-icon>
             </f7-list-item>
             <f7-list-item link="/dynamic-route/blog/11/post/23/" title="Dynamic Route">
-                <f7-icon slot="media" material="star"></f7-icon>
+                <f7-icon slot="media" icon="far fa-star fa-fw"></f7-icon>
             </f7-list-item>
             <f7-list-item link="/about/" title="About">
-                <f7-icon slot="media" material="info_outline"></f7-icon>
+                <f7-icon slot="media" icon="fas fa-info fa-fw"></f7-icon>
             </f7-list-item>
             <f7-list-item link="/tabs-routable/" title="Routable Tabs">
-                <f7-icon slot="media" material="tab" color="blue" size=""></f7-icon>
+                <f7-icon slot="media" icon="fas fa-th-large fa-fw" color="blue"></f7-icon>
             </f7-list-item>
         </f7-list>
         <f7-list class="searchbar-not-found">
--- src/pages/tabs-routable.vue
+++ src/pages/tabs-routable.vue
@@ -3,18 +3,21 @@
         <f7-navbar title="Routable Tabs" back-link="Back" sliding></f7-navbar>
         <f7-toolbar tabbar>
             <a href="./" class="tab-link" data-route-tab-id="tab1">
-              <i class="icon material-icons">home</i>
+              <i class="icon fas fa-home fa-2x"></i>
               <span class="tabbar-label">Home</span>
             </a>
             <a href="tab2/" class="tab-link" data-route-tab-id="tab2">
-              <i class="icon material-icons size-50">today<span class="badge color-red">5</span></i>
+              <span class="fa-layers fa-2x">
+                <i class="fas fa-calendar-alt"></i>
+                <span class="fa-layers-counter fa-lg">99</span>
+              </span>
               <span class="tabbar-label">Calendar</span>
             </a>
             <a href="tab3/" class="tab-link" data-route-tab-id="tab3">
-              <i class="icon material-icons">perm_identity</i>
+              <i class="icon fas fa-user fa-2x"></i>
               <span class="tabbar-label">Profile</span>
             </a>
-<!--             <f7-link tab-link href="tab3/" route-tab-id="tab3">Tab 3</f7-link> -->
+            <!--             <f7-link tab-link href="tab3/" route-tab-id="tab3">Tab 3</f7-link> -->
         </f7-toolbar>
         <f7-tabs routable>
             <f7-tab class="page-content hide-navbar-on-scroll" id="tab1"></f7-tab>
@@ -25,7 +28,11 @@
 </template>
 <style>
 .md .navbar+.toolbar:not(.toolbar-bottom-md):not(.messagebar) {
-    top: 56px;
+  top: 56px;
+}
+.fa-layers-counter {
+    height: inherit;
+    right: -0.2em;
 }
 </style>
 <script>
